6c0c8d35185fcfa260b5f06760b03aa7ab4d82bb
MBHallgren <s153002@student.dtu.dk>
2023-09-07 15:56:34 +0200

    push2

--- outbreak_maker/denovoAssembly.py
+++ outbreak_maker/denovoAssembly.py
@@ -1,10 +1,18 @@
 import os
 import sys
 
+from outbreak_maker import draftGenome
+
 def assemble_illumina_reads(args):
     "Runs spades.py to assemble illumina data"
-    cmd = 'spades.py -1 {} -2 {} -o {}/assembly --threads 8'.format(args.illumina[0], args.illumina[1], args.output)
+    name = args.illumina[0].split('/')[-1].split('.')[0]
+    if not os.path.exists(args.output + '/' + name):
+        os.system('mkdir -p {}/{}'.format(args.output, name))
+    cmd = 'spades.py -1 {} -2 {} -o {}/{}/assembly --threads 8'.format(args.illumina[0], args.illumina[1], args.output, name)
     os.system(cmd)
+    draft_genome = draftGenome.concat_draft_genome(args, name, '{}/{}/assembly/scaffolds.fasta'.format(args.output, name))
+    print (draft_genome)
+    sys.exit()
     # TBD
     return args.epi_dict
 
---
+++ outbreak_maker/draftGenome.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+def concat_draft_genome(args, name, scaffold_file):
+    sequence = ''
+    header = '>concatenated_draft_genome_of_assembly:{}'.format(name)
+    with open(scaffold_file, 'r') as f:
+        for line in f:
+            if not line.startswith('>'):
+                line = line.strip()
+                sequence += line
+
+    with open('{}/{}/{}'.format(args.output, name, 'draft_genome.fasta'), 'w') as f:
+        print (header, file = f)
+        print (sequence, file = f)
+
+    return '{}/{}/{}'.format(output, name, 'draft_genome.fasta')
