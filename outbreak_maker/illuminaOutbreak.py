f5c2810453a2971985acf2306d44a8f2eabb9829
MBHallgren <s153002@student.dtu.dk>
2023-09-08 10:49:08 +0200

    push14

--- outbreak_maker/illuminaOutbreak.py
+++ outbreak_maker/illuminaOutbreak.py
@@ -5,18 +5,23 @@ from outbreak_maker import utils
 from outbreak_maker import evalKmaResults
 
 def determine_illumina_outbreak(args):
-    cmd = 'kma -ipe {} {} -o {}/reference_templates -t_db {} -t {} -mem_mode -Sparse'\
-        .format(args.illumina[0], args.illumina[1], args.output,args.epi_dict['database'], args.threads)
-    print (cmd)
-    os.system(cmd)
-    spa_file = '{}/reference_templates.spa'.format(args.output)
-    template_number, template_score, reference_header_text = utils.find_best_template_from_spa_file(spa_file, args.epi_dict['database'])
-    print (template_number, template_score, reference_header_text)
+    for i in range(0, len(args.illumina), 2):
+        name = args.illumina[i].split('/')[-1].split('.')[0]
+        print (args.output)
+        cmd = 'kma -ipe {} {} -o {}/{}/reference_templates -t_db {} -t {} -mem_mode -Sparse'\
+            .format(args.illumina[i], args.illumina[i+1], args.output, name, args.epi_dict['database'], args.threads)
+        print (cmd)
+        os.system(cmd)
+        spa_file = '{}/{}/reference_templates.spa'.format(args.output, anme)
+        template_number, template_score, reference_header_text = utils.find_best_template_from_spa_file(spa_file, args.epi_dict['database'])
+        print (template_number, template_score, reference_header_text)
 
-    os.system('kma -ipe {} {} -o {} -t_db {} -mint3 -Mt1 {} -t {}'
-              .format(args.illumina[0], args.illumina[1], args.output, args.epi_dict['database'], template_number, args.threads))
-
-    sys.exit()
+        #Determine ANI to best template
+        cmd = 'kma -ipe {} {} -o {}/{} -t_db {} -mint3 -Mt1 {} -t {}'
+                 .format(args.illumina[i], args.illumina[i+1], args.output, name, args.epi_dict['database'], template_number, args.threads)
+        print (cmd)
+        os.system(cmd)
+        sys.exit()
 
     cluster_id, score = evalKmaResults.derive_kma_alignment_results()
 
