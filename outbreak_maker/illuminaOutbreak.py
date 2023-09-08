55e86b29d3d48a0c517808477682c149183ddec2
MBHallgren <s153002@student.dtu.dk>
2023-09-08 11:07:07 +0200

    push17

--- outbreak_maker/illuminaOutbreak.py
+++ outbreak_maker/illuminaOutbreak.py
@@ -8,11 +8,11 @@ def determine_illumina_outbreak(args):
     for i in range(0, len(args.illumina), 2):
         name = args.illumina[i].split('/')[-1].split('.')[0]
         print (args.output)
-        cmd = 'kma -ipe {} {} -o {}/{}/reference_templates -t_db {} -t {} -mem_mode -Sparse'\
-            .format(args.illumina[i], args.illumina[i+1], args.output, name, args.epi_dict['database'], args.threads)
+        cmd = 'kma -ipe {} {} -o {}/reference_templates -t_db {} -t {} -mem_mode -Sparse'\
+            .format(args.illumina[i], args.illumina[i+1], args.output, args.epi_dict['database'], args.threads)
         print (cmd)
         os.system(cmd)
-        spa_file = '{}/{}/reference_templates.spa'.format(args.output, name)
+        spa_file = '{}/reference_templates.spa'.format(args.output)
         template_number, template_score, reference_header_text = utils.find_best_template_from_spa_file(spa_file, args.epi_dict['database'])
         print (template_number, template_score, reference_header_text)
 
